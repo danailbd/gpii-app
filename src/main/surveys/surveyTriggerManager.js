19a31884396132cf19b011fee8ef2aabb9d45494
Danail Dimitrov <dandimitrov@asteasolutions.com>
2017-12-12 21:00:54 +0200

    GPII-2732: Initial idea for the Trigger Manger

--- src/main/surveys/surveyTriggerManager.js
+++ src/main/surveys/surveyTriggerManager.js
@@ -93,3 +93,245 @@ gpii.app.surveyTriggersManager.clearTriggersIfNeeded = function (that, keyedInUs
         that.clearTriggers();
     }
 };
+
+
+/*
+    {
+        type: "keyedInBefore",
+        value: {
+            unit: "m",
+            value: 5
+        }
+    },
+    {
+        type: "keyedInLessThan",
+        value: {
+            unit: "h",
+            value: 2
+        }
+    }
+
+
+Manager is always created
+    * has multiple handler for each type of condition
+Manager splits trigger registration into multiple handlers
+
+
+Handlers are singletons 
+Have names
+They are created in the start (inital initialization)
+Provides facts asyncly
+Have simple interface
+    Fire event on fact state?
+    Can be cleared (reset), stopping timers etc.
+
+Timer - self sufficant setTimeout/interval wrapper
+Notifies for timeout/interval appear (event vs callback)
+May re
+
+
+
+keyedInBeforeHandler
+    - listens for keyIn
+        - sets local timestamp propery
+    - registers timer
+    - clears timer
+
+*/
+
+fluid.defaults("gpii.app.surveyTriggersManagerV2", {
+    gradeNames: ["fluid.modelComponent"],
+
+    model: {
+    },
+
+    events: {
+        onTriggerOccurred: null
+    },
+
+    members: {
+        // single instance handlers
+        handlersMap: {
+            keyedInBefore: "{that}.keyedInBeforeHandler",
+            // more fun
+        }
+    },
+
+    listeners: {
+        "{app}.events.onKeyOut": "{that}.reset"
+    },
+
+    components: {
+        // IDEA: separate component - triggersEngine
+        // condition handlers
+        userKeyedInBeforeHandler: {
+            // registers
+            type: "gpii.app.surveyTriggersManagerV2.userKeyedInBeforeHandler"
+        }
+    },
+
+    invokers: {
+        reset: {
+            funcName: "gpii.app.surveyTriggersManagerV2.reset",
+            args: "{that}"
+        },
+        registerTrigger: {
+            funcName: "gpii.app.surveyTriggersManagerV2.registerTrigger",
+            args: [
+                "{that}.handlersMap",
+                "{arguments}.0"
+            ]
+        }
+    }
+});
+
+
+gpii.app.surveyTriggersManagerV2.registerTrigger = function (handlers, triggerData) {
+    /// TODO support:
+    /// * ANY, ALL
+
+    // * iter conditions
+    // * apply handler for conditions
+};
+
+
+////////////////////////////////////////////////////////////////////////
+//                              HANDLERS                              //
+////////////////////////////////////////////////////////////////////////
+fluid.defaults("gpii.app.surveyTriggersManagerV2.baseHandler", {
+    gradeNames: ["fluid.modelComponent", "gpii.app.surveyTriggersManagerV2.baseHandler"],
+
+    name: "", // Added by implementor
+
+    events: {
+        // TODO better name? / of component
+        onSimpleTrigger: null
+    },
+
+    invokers: {
+        handle: {
+            funcName: "fluid.notImplemented",
+            args: ["{arguments}.0"]
+        },
+        reset: {
+            funcName: "fluid.notImplemented"
+        }
+    }
+});
+
+fluid.defaults("gpii.app.surveyTriggersManagerV2.keyedInBeforeHandler", {
+    gradeNames: ["gpii.app.surveyTriggersManagerV2.baseHandler"],
+    name: "keyedInBeforeHandler",
+
+    model: {
+        // TODO could be extracted to upper component
+        keyedInTimestamp: null,
+    },
+
+    listener: {
+        "{app}.events.keyedIn": {
+            func: "{that}.updateKeyedInTimestamp"
+        }
+    },
+
+    components: {
+        timer: {
+            type: "gpii.app.timer",
+            events: {
+                // Just make an alias
+                onTimerFinished: "{that}.events.onSimpleTrigger"
+            }
+        }
+    },
+
+    invokers: {
+        handle: {
+            funcName: "gpii.app.surveyTriggersManager.keyedInBeforeHandler.handle",
+            args: [
+                "{that}.model.keyedInTimestamp",
+                "{that timer}",
+                "{arguments}.0"
+            ]
+        },
+        reset: {
+            funcName: "{that timer}.clear"
+        },
+
+        updateKeyedInTimestamp: {
+            changePath: "keyedInUserTimestamp",
+            value: "@expand:Date.now()"
+        }
+    }
+});
+
+gpii.app.surveyTriggersManager.keyedInBeforeHandler.handle =
+    function (keyedInTimestamp, timer, condition) {
+        var unit = condition.value.unit,
+            value = condition.value.value;
+
+        var timeout = 0;
+        switch (unit) {
+        // TODO enum? (compute prefix only once)
+        case "s":
+            timeout = 1000 * value;
+            break;
+        case "m":
+            timeout = 1000 * 60 * value;
+            break;
+        case "h":
+            timeout = 1000 * 60 * 60 * value;
+            break;
+        default:
+            console.log("ERROR(KeyedInBeforeHandler): No such unit - ", unit);
+        }
+
+        // As the keyed in may have occurred some time before the
+        // triggers are registered, ensure proper timer since
+        // "keyed in" is passed
+        timer.start(timeout - (Date.now() - keyedInTimestamp));
+    };
+
+
+/*
+    TODO
+    Two posible approaches:
+    * fire event onTimerFinished
+ */
+fluid.defaults("gpii.app.timer", {
+    gradeNames: ["fluid.modelComponent"],
+
+    model: {
+        timer: null,
+
+        /// could be useful
+        // value: null,
+        // unit: null
+    },
+
+    listeners: {
+        "onDestroy.clearTimer": "{that}.clear"
+    },
+
+    events: {
+        onTimerFinished: null
+    },
+
+    invokers: {
+        start: {
+            funcName: "gpii.app.timer.start",
+            args: [
+                "{that}",
+                "{arguments}.0"
+            ]
+        },
+        clear: {
+            funcName: "clearTimeout",
+            args: "{that}.model.timer"
+        }
+    }
+});
+
+gpii.app.timer.start = function (timer, timeout) {
+    timer.change.applier("timer",
+        setTimeout(timer.events.onTimerFinished.fire, timeout));
+};
