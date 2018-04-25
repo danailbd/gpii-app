d07711d7d71c54bbd3aeac54827aaa7afa40a20b
Danail Dimitrov <dandimitrov@asteasolutions.com>
2018-04-25 18:38:32 +0300

    GPII-2969: Improve sign in component

--- src/renderer/psp/js/signin.js
+++ src/renderer/psp/js/signin.js
@@ -28,74 +28,165 @@
         gradeNames: ["fluid.viewComponent", "gpii.psp.heightObservable"],
 
         model: {
-            messages:            {
-                titleIntro:      "GPII Auto-personalization",
-                subtitleIntro:   "Automatically set up any computer the way you want it.",
+            messages: {
+                signinHeader:         "Not keyed in",
 
-                title:           "Use your GPII Key to key-in",
-                subtitle:        "Or key-in with your account below"
+                titleIntro:           "GPII Auto-personalization",
+                subtitleIntro:        "Automatically set up any computer the way you want it.",
+
+                title:                "Use your GPII Key to key-in",
+                subtitle:             "Or key-in with your account below",
+
+                emailTextInputLabel:  "Email",
+                passwordInputLabel:   "Password",
+                signinButton:         "Sign in",
+                signupButton:         "Create an Account",
+                forgotPasswordButton: "Forgot Password?"
             },
 
             email: null,
-            password: null, // TODO set when
+            password: null,
 
             error: {
-                title: "Wrong name or password",
-                details: "Try again or use a key"
+                title: null,
+                details: null
             }
         },
 
         selectors: {
-            titleIntro:    ".flc-contentTitleIntro",
-            subtitleIntro: ".flc-contentSubTitleIntro",
+            signinHeader:         ".flc-signinHeader",
 
-            title:         ".flc-contentTitle",
-            subtitle:      ".flc-contentSubtitle",
+            titleIntro:           ".flc-contentTitleIntro",
+            subtitleIntro:        ".flc-contentSubTitleIntro",
 
+            title:                ".flc-contentTitle",
+            subtitle:             ".flc-contentSubtitle",
+
+            emailTextInputLabel:  ".flc-emailTextInput-label",
+            passwordInputLabel:   ".flc-passwordInput-label",
+
+            emailTextInput:       ".flc-emailTextInput",
+            passwordInput:        ".flc-passwordInput",
+
+            signupButton:         ".flc-signupBtn",
+            forgotPasswordButton: ".flc-forgotPasswordBtn",
+
+            // shorten name to avoid text setting
+            signinBtn:            ".flc-signinBtn",
+
+            error:                ".flc-error",
+            errorTitle:           ".flc-errorTitle",
+            errorDetails:         ".flc-errorDetails"
         },
 
         events: {
-            onButtonClicked: null
+            onSigninClicked: null,
+            onSigninRequested: null
+        },
+
+        invokers: {
+            updateError: {
+                changePath: "error",
+                value: "{arguments}.0"
+            }
+        },
+
+        listeners: {
+            onSigninClicked: {
+                funcName: "{that}.events.onSigninRequested",
+                args: [
+                    {
+                        expander: {
+                            this: "{that}.dom.emailTextInput",
+                            method: "val"
+                        }
+                    }, {
+                        expander: {
+                            this: "{that}.dom.passwordInput",
+                            method: "val"
+                        }
+                    }
+                ]
+            },
+
+            // XXX test
+            onSigninRequested: [{
+                this: "console",
+                method: "log",
+                args: ["Sign in requested: ", "{arguments}.0", "{arguments}.1"]
+            }, {
+                func: "{that}.updateError",
+                args: [{
+                    title: "Wrong name or password",
+                    details: "Try again or use a key"
+                }]
+            }]
         },
 
         modelListeners: {
             // Any change means that the whole view should be re-rendered
-            "": {
+            "messages": {
                 funcName: "gpii.psp.signin.renderText",
                 args: [
-                    "{that}.dom",
+                    "{that}",
                     "{that}.options.selectors",
                     "{that}.model.messages"
                 ]
+            },
+            "error": {
+                funcName: "gpii.psp.signin.toggleError",
+                args: [
+                    "{that}",
+                    "{change}.value"
+                ]
+            }
+        },
+
+        components: {
+            singinBtn: {
+                type: "gpii.psp.widgets.button",
+                container: "{that}.dom.signinBtn",
+                options: {
+                    attrs: {
+                        "aria-label": "{signin}.model.messages.signinButton"
+                    },
+                    model: {
+                        label: "{signin}.model.messages.signinButton"
+                    },
+                    invokers: {
+                        "onClick": "{signin}.events.onSigninClicked.fire"
+                    }
+                }
             }
         }
     });
 
+    gpii.psp.signin.toggleError = function (that, error) {
+        var errorContainer = that.dom.locate("error").show();
+        if (!error.title) {
+            errorContainer.hide();
+        } else {
+            errorContainer.show();
 
-    /**
-     * TODO
-     *
-     * @param dom
-     * @param messages
-     */
-    gpii.psp.signin.renderText = function (dom, selectors, messages) {
-        fluid.each(selectors, function (value, key) {
-            var el = dom.locate(key);
-            if (el) {
-                el.text(messages[key]);
-            }
-        });
+            that.dom.locate("errorTitle").text(error.title);
+            that.dom.locate("errorDetails").text(error.details);
+        }
     };
 
 
     /**
-     * Construct user listeners text.
+     * Set text to dom items in case there is a message for
+     * its viewComponent's selector.
      *
-     * @param description {String} The description with a placeholder by the name "items"
-     * @param userListeners {String[]} The list of key in listeners for the user
-     * @returns {String} The constructed string
+     * @param {Component} that - The `gpii.psp.signin` instance.
+     * @param {Object} messages - The translated text
      */
-    gpii.psp.signin.getUserListenersText = function (description, userListeners) {
-        return fluid.stringTemplate(description, { listeners: userListeners.join(", ") });
+    gpii.psp.signin.renderText = function (that, selectors, messages) {
+        fluid.each(selectors, function (value, key) {
+            var el = that.dom.locate(key);
+            if (el && messages[key]) {
+                el.text(messages[key]);
+            }
+        });
     };
 })(fluid);
