aea4a5081c4ff6aa2fd273de64c97696a63f3413
Danail Dimitrov <dandimitrov@asteasolutions.com>
2018-12-27 14:15:30 +0200

    GPII-3045: Update files' licence header

--- src/renderer/aboutDialog/js/aboutDialog.js
+++ src/renderer/aboutDialog/js/aboutDialog.js
@@ -2,12 +2,18 @@
  * The about dialog
  *
  * Represents an about dialog that can be closed by the user.
- * Copyright 2017 Raising the Floor - International
+ * Copyright 2018 Raising the Floor - US
  *
  * Licensed under the New BSD license. You may not use this file except in
  * compliance with this License.
- * The research leading to these results has received funding from the European Union's
- * Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
+ * The R&D that led to this software was funded by the Rehabilitation Services Administration,
+ * US Dept of Education under grant H421A150006 (APCP), by the National Institute on Disability,
+ * Independent Living, and Rehabilitation Research (NIDILRR), US Administration for
+ * Independent Living & US Dept of Education under Grants H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00
+ * (UIITA-RERC), by the European Union's Seventh Framework Programme (FP7/2007-2013) grant agreement n° 289016 (Cloud4all)
+ * and 610510 (Prosperity4All), by the Flora Hewlett Foundation, the Ontario Ministry of Research and Innovation,
+ * and the Canadian Foundation for Innovation, by Adobe Foundation and the Consumer Electronics Association Foundation.
+ * The opinions and results herein are those of the authors and not necessarily those of the funding agencies.
  * You may obtain a copy of the License at
  * https://github.com/GPII/universal/blob/master/LICENSE.txt
  */
